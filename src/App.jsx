7a5057028439ce4c565b73de9edd09f2d8d0c959
TaniaTabassum-22 <taniatabassum952@gmail.com>
2025-10-28 15:58:04 +0530

    Task Management App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import TaskForm from "./Components/TaskForm";
 import TaskList from "./Components/TaskList";
 import ProgressTracker from "./Components/ProgressTracker";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  });
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
+
   return (
     <div>
       <h1>Task Buddy&#x1F3AF;</h1>
       <p>Your Personal Productivity Partner&#x1F91D;</p>
 
-      <TaskForm />
+      <TaskForm addTask = {addTask}/>
       <TaskList />
       <ProgressTracker />
 
